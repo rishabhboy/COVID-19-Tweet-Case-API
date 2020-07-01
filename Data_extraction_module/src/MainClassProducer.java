cc44c16816996ccb4db9ecd44165be9235d80635
RISHABHVARSH <34577015+RISHABHVARSH@users.noreply.github.com>
2020-07-01 22:54:00 +0530

    Update MainClassProducer.java

--- Data_extraction_module/src/MainClassProducer.java
+++ Data_extraction_module/src/MainClassProducer.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 public class MainClassProducer {
     public static void main(String[] args) throws IOException, ParseException {
-        String consumerKey = "VWbXRNR0XnNzfXok0Bl3fngMJ";
-        String consumerSecret = "brvGyuCxDQ177liijclvnxInjcedINXUkCnIJrDOwu0LreSSon";
-        String token = "1246568392720449536-2kPcc7aciDP66V4FSyJ9QqLfzGBGkv";
-        String secret = "BrjDm3gUAXX3zlrQFo9dU1zRocjMolvJXZuPsQHsuE7wk";
+        String consumerKey = "";
+        String consumerSecret = "";
+        String token = "";
+        String secret = "";
         String bootstrapServers = "127.0.0.1:9092";
         String topic = "tweet_26_topic";
         List<String> keywords = Lists.newArrayList("corona","Covi19","Virus");
